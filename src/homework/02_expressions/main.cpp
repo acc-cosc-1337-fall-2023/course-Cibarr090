f3071455ab640112fcd01b09af57556eee1a11d5
Cibarr090 <143145172+Cibarr090@users.noreply.github.com>
2023-09-10 00:36:04 +0000

    Homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,35 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include "iostream"
 
 //write namespace using statement for cout
 
+using std::cout; using std::cin;
+
+
 int main()
 {
-	
+	double meal_amount; double tip_rate; double tip_amount; double tax_amount; double total;
+
+	cout<<"Meal Amount: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+
+	cout<<"Tip Rate: ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"Total: "<<total<<"\n";
+
+
 
 	return 0;
 }
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -12,3 +12,14 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Test Get Sales Tax function") 
+{
+	REQUIRE(get_sales_tax_amount(10));
+	REQUIRE(get_sales_tax_amount(20));
+}
+
+TEST_CASE("Test Get Tip Amount function") 
+{
+	REQUIRE(get_tip_amount(20, .15));
+	REQUIRE(get_tip_amount(20, .2));
+}
