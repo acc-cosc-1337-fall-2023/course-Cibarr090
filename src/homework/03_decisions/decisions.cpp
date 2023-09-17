e35d1a7635e743c8d79173e1342c9e87fe378936
Cibarr090 <143145172+Cibarr090@users.noreply.github.com>
2023-09-17 03:46:02 +0000

    decisions test

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,68 @@
 //write include statement for decisions header
+#include "decisions.h"
 
+using std::string;
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string result;
+
+    if(grade >= 90 && grade <= 100)
+    {
+        result = "A";
+    }
+    else if(grade >= 80 && grade <=89)
+    {
+        result = "B";
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        result = "C";
+    }
+    else if(grade >= 60 && grade <=69)
+    {
+        result = "D";
+    }
+    else if(grade >= 50 && grade <= 59)
+    {
+        result = "F";
+    }
+    else
+    {
+        result = "Number out of range";
+    }
+
+    return result;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string result;
+
+    switch(grade)
+    {
+    
+    case 95:
+        result = 'A';
+        break;
+    case 85:
+        result = 'B';
+        break;
+    case 75:
+        result = 'C';
+        break;
+    case 65:
+        result = 'D';
+        break;
+    case 50:
+        result = "F";
+        break;
+    default:
+        result = "Number out of range";
+        break;
+    }
+        
+    return result;
+
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,32 @@
-//write include statements
+#include "decisions.h"
+
+using std::cin; using std::cout; using std::string;
 
 int main() 
 {
+	cout<<" \t Main Menu\n\n";
+	cout<<"1-Letter grade using if\n";
+	cout<<"2-Letter grade using switch\n";
+	cout<<"3-Exit\n";
+	
+	
+	auto option = 0;
+	cout<<"Enter number: ";
+	cin>>option;
+
+	switch(option)
+	{
+	case 1:
+		string get_letter_grade_using_if(int grade);
+		break;
+	case 2:
+		string get_letter_grade_using_switch(int grade);
+		break;
+	default:
+		cout<<"Exit\n";
+		break;
+	}
+	
+
 	return 0;
 }
