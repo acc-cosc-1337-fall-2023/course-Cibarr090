e15d0f9a617602c07ee2dd0e7ae2ac9b5cabb2e3
Cibarr090 <143145172+Cibarr090@users.noreply.github.com>
2023-09-30 21:44:18 +0000

    vector capacity, size, and push_back

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -10,26 +10,19 @@ int main()
     cout<<"Capacity: "<<nums.capacity()<<"\n";
     cout<<"Size: "<<nums.size()<<"\n";
 
-    vector<int> nums1;
-    vector<int> nums2(5, 10);
-
     for(auto i=0; i < nums.size(); i++)
-    for(auto num2: nums2)
     {
         cout<<nums[i]<<"\n";
-        cout<<num2<<"\n";
     }
     cout<<"\n";
     nums.push_back(3);
     cout<<"Capacity: "<<nums.capacity()<<"\n";
     cout<<"Size: "<<nums.size()<<"\n";
-    vector<int> nums3 = nums;
-
+    
     for(auto num: nums)
-    for(auto num3: nums3)
+    
     {
         cout<<num<<"\n";
-        cout<<num3<<"\n";
     }
     cout<<"\n";
     nums.push_back(15);
@@ -40,14 +33,10 @@ int main()
 
     nums.push_back(0);
 
-    vector<double> doubles {5.5, 3.9, 8.99};
     cout<<"\n";
     cout<<"Capacity: "<<nums.capacity()<<"\n";
     cout<<"Size: "<<nums.size()<<"\n";
-    for(auto dub: doubles)
-    {
-        cout<<dub<<"\n";
-    }
+    
     
     return 0;
 }
